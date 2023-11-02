d796003e178be344f6fe6574babb4e959f695a07
Scott Smyth <smythscott60@gmail.com>
2023-11-02 16:41:44 +0000

    - Add logic to handle inviting users to view task in Task.js - Add list to display users with access

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { axiosRequest } from "../api/axiosDefaults";
-import SpinnerButton from './Spinner';
-import { Link } from 'react-router-dom';
-import DeleteTaskModal from './DeleteTaskModal';
-import UserSearch from './UserSearch'; 
-import { toast } from "react-toastify"; 
+import SpinnerButton from "./Spinner";
+import { Link } from "react-router-dom";
+import DeleteTaskModal from "./DeleteTaskModal";
+import UserSearch from "./UserSearch";
+import { toast } from "react-toastify";
 
 function Task() {
   const { taskId } = useParams();
@@ -13,21 +13,35 @@ function Task() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axiosRequest.get(`/tasks/${taskId}/`)
+    axiosRequest
+      .get(`/tasks/${taskId}/`)
       .then((response) => {
         setTask(response.data);
         setLoading(false);
-        
       })
       .catch((error) => {
-        console.error('Error fetching tasks:', error);
+        console.error("Error fetching tasks:", error);
       });
   }, [taskId]);
 
-  const handleUserSelect = (user) => {
-    //  need to update the tasks shared users field to include the invitd user
-    console.log(" test Inviting user to task:", user.username);
-    
+  const handleUserSelect = (selectedUser) => {
+    const updatedUsernames = Array.isArray(task.shared_users_usernames)
+      ? [...task.shared_users_usernames, selectedUser.username]
+      : [selectedUser.username];
+
+    axiosRequest
+      .patch(`/tasks/${taskId}/`, {
+        shared_users_usernames: updatedUsernames,
+      })
+      .then((response) => {
+        console.log(response.data);
+        setTask(response.data);
+        toast.success(`${selectedUser.username} has been invited to this event`);
+      })
+      .catch((error) => {
+        console.error("Error updating task shared users:", error);
+        toast.error(` Error adding ${selectedUser.username}, to this event`);
+      });
   };
 
   if (loading) {
@@ -43,12 +57,18 @@ function Task() {
       <p>Image:</p>
       {task.image && <img src={task.image} alt="Task" />}
 
+      <p>Shared Users:</p>
+      <ul>
+        {task.shared_users_usernames.map((username) => (
+          <li key={username}>{username}</li>
+        ))}
+      </ul>
+
       <Link to={`/editTask/${task.id}`}>
         <button>Edit Task</button>
       </Link>
       <DeleteTaskModal taskId={taskId} />
 
-    
       <div>
         <h3>Invite Users to View Task</h3>
         <UserSearch onSelectUser={handleUserSelect} />
