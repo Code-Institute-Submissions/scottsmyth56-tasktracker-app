f2422c89e51e013769bba43f8a2b5262436ac187
Scott Smyth <smythscott60@gmail.com>
2023-11-06 10:36:32 +0000

    - Add Filtering by status and priority on Task Page

--- src/pages/tasks/TaskPage.js
+++ src/pages/tasks/TaskPage.js
@@ -1,48 +1,76 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import {axiosRequest} from '../../api/axiosDefaults';
-import { useCurrentUser } from '../../contexts/UserContext';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { axiosRequest } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/UserContext";
+import { useNavigate } from "react-router-dom";
+
 function TaskPage() {
   const [tasks, setTasks] = useState([]);
+  const [statusFilter, setStatusFilter] = useState("all");
+  const [priorityFilter, setPriorityFilter] = useState("all");
+
   const currentUser = useCurrentUser();
   const history = useNavigate();
 
   function handleLogout() {
-    localStorage.removeItem('authToken');
-    console.log("logged out" + localStorage.getItem('authToken')); 
-    history('/login'); 
+    localStorage.removeItem("authToken");
+    console.log("logged out" + localStorage.getItem("authToken"));
+    history("/login");
   }
 
-
   useEffect(() => {
-    axiosRequest.get('/tasks/') 
+    axiosRequest
+      .get("/tasks/")
       .then((response) => {
-        // console.log(response.data)
-        setTasks(response.data); 
+        setTasks(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching tasks:', error);
+        console.error("Error fetching tasks:", error);
       });
   }, []);
+
+  const filteredTasks = tasks.filter((task) => {
+    return (
+      (statusFilter === "all" || task.status === statusFilter) &&
+      (priorityFilter === "all" || task.priority === priorityFilter)
+    );
+  });
 
   return (
     <div>
       <h1>Task List</h1>
+      <div>
+        <label>Status:</label>
+        <select onChange={(e) => setStatusFilter(e.target.value)}>
+          <option value="all">All</option>
+          <option value="todo">To Do</option>
+          <option value="inprogress">In Progress</option>
+          <option value="done">Done</option>
+        </select>
+      </div>
+      <div>
+        <label>Priority:</label>
+        <select onChange={(e) => setPriorityFilter(e.target.value)}>
+          <option value="all">All</option>
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        </select>
+      </div>
       <ul>
-        {tasks.map((task) => (
+        {filteredTasks.map((task) => (
           <li key={task.id}>
             <Link to={`/tasks/${task.id}`}>{task.title}</Link>
           </li>
         ))}
       </ul>
-      <h2>Welcome, {currentUser?.username || 'Guest'}</h2>
-       <button onClick={handleLogout}>Logout</button> 
-       <Link to="/register">
-      <button> Register</button>
+      <h2>Welcome, {currentUser?.username || "Guest"}</h2>
+      <button onClick={handleLogout}>Logout</button>
+      <Link to="/register">
+        <button>Register</button>
       </Link>
       <Link to="/createTask">
-      <button>Create Task + </button>
+        <button>Create Task +</button>
       </Link>
     </div>
   );
