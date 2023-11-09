567ce03c124ff8f163680b580f806fe93fdb08a2
Scott Smyth <smythscott60@gmail.com>
2023-11-09 00:04:18 +0000

    - Update layout for Create Event - Add Toast Alert for creating event

--- src/pages/events/CreateEventForm.js
+++ src/pages/events/CreateEventForm.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import { axiosRequest } from '../../api/axiosDefaults';
 import { useNavigate } from 'react-router-dom';
+import styles from '../../styles/CreateEventForm.module.css';
+import { toast } from 'react-toastify';
 
 function CreateEvent() {
     const history = useNavigate();
-
 
     const [eventData, setEventData] = useState({
         title: '',
@@ -24,75 +25,84 @@ function CreateEvent() {
         e.preventDefault();
 
         try {
-            await axiosRequest.post('/events/', eventData, {});
+            await axiosRequest.post('/events/', eventData);
             console.log('Event data:', eventData);
             history('/events');
-
+            toast.success(`Event created sucesfully`, {
+                position: toast.POSITION.TOP_CENTER,
+                autoClose: 3000,
+            });
         } catch (error) {
             console.error('Error:', error);
         }
     };
 
     return (
-        <div>
-            <h1>Create Event</h1>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="title">
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control
-                        type="text"
-                        name="title"
-                        value={eventData.title}
-                        onChange={handleInputChange}
-                    />
-                </Form.Group>
+        <Container className={styles["create-event-container"]}>
+            <Row className="justify-content-md-center">
+                <Col xs={12} md={8} lg={6}>
+                    <h1 className="text-center text-white">Create Event</h1>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="title" className="mb-3">
+                            <Form.Label className="text-white">Title</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="title"
+                                value={eventData.title}
+                                onChange={handleInputChange}
+                            />
+                        </Form.Group>
 
-                <Form.Group controlId="description">
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control
-                        as="textarea"
-                        rows={3}
-                        name="description"
-                        value={eventData.description}
-                        onChange={handleInputChange}
-                    />
-                </Form.Group>
+                        <Form.Group controlId="description" className="mb-3">
+                            <Form.Label className="text-white">Description</Form.Label>
+                            <Form.Control
+                                as="textarea"
+                                rows={3}
+                                name="description"
+                                value={eventData.description}
+                                onChange={handleInputChange}
+                            />
+                        </Form.Group>
 
-                <Form.Group controlId="date">
-                    <Form.Label>Date</Form.Label>
-                    <Form.Control
-                        type="date"
-                        name="date"
-                        value={eventData.date}
-                        onChange={handleInputChange}
-                    />
-                </Form.Group>
+                        <Form.Group controlId="date" className="mb-3">
+                            <Form.Label className="text-white">Date</Form.Label>
+                            <Form.Control
+                                type="date"
+                                name="date"
+                                value={eventData.date}
+                                onChange={handleInputChange}
+                            />
+                        </Form.Group>
 
-                <Form.Group controlId="time">
-                    <Form.Label>Time</Form.Label>
-                    <Form.Control
-                        type="time"
-                        name="time"
-                        value={eventData.time}
-                        onChange={handleInputChange}
-                    />
-                </Form.Group>
+                        <Form.Group controlId="time" className="mb-3">
+                            <Form.Label className="text-white">Time</Form.Label>
+                            <Form.Control
+                                type="time"
+                                name="time"
+                                value={eventData.time}
+                                onChange={handleInputChange}
+                            />
+                        </Form.Group>
 
-                <Form.Group controlId="location">
-                    <Form.Label>Location</Form.Label>
-                    <Form.Control
-                        type="text"
-                        name="location"
-                        value={eventData.location}
-                        onChange={handleInputChange}
-                    />
-                </Form.Group>
+                        <Form.Group controlId="location" className="mb-3">
+                            <Form.Label className="text-white">Location</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="location"
+                                value={eventData.location}
+                                onChange={handleInputChange}
+                            />
+                        </Form.Group>
 
-                <Button variant="primary" type="submit">
-                    Create Event
-                </Button>
-            </Form>
-        </div>
+                        <div className="d-grid gap-2">
+                            <Button variant="primary" type="submit">
+                                Create Event
+                            </Button>
+                        </div>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
